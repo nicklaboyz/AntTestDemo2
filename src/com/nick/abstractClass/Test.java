39ad6f6bf6cdc2c9d251a4a12b67a5a9cc20892c
Administrator <Administrator@WIN-CK7DF2BFGTN>
2019-01-22 13:42:40 +0800

    again upload

--- src/com/nick/abstractClass/Test.java
+++ src/com/nick/abstractClass/Test.java
@@ -3,7 +3,7 @@ package com.nick.abstractClass;
 public class Test {
 
 	public static void main(String[] args) {
-//		Animal a = new Animal();  报错信息
+//		Animal a = new Animal();  报错信息处理
 		Animal a = new Cat();   //建立姿类对象，父类对象指象子类引用
 
 	}
